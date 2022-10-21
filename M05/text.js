b548c933cb687cb4cde35970b9d788347c6bf6f2
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-10-20 23:46:57 -0700

    Update text.js

--- M05/text.js
+++ M05/text.js
@@ -1,21 +1,19 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';      // Message
 
 function myFunction(){
-  var num = prompt("Please enter a number", "1");
-  if (operator === 'multiplication') {
-    // Do multiplication
-    while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
+  var num = prompt("Please enter a number:");
+  if (0<num<10){
+  while(i<11){
+   msg += i+'x'+num+'='+(i*num)+'<br/>';
+   i++;
   }
-} else {
+  }else{
     alert('Invalid input!');
-}
+  }
 }
 
 // Write the message into the page
-var el = document.getElementById('blackboard');
-el.innerHTML = msg;
+document.getElementById('blackboard').innerHTML =msg;
+ console.log(myFunction());
