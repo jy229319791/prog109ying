715538251df1a91a8185d4e278717aebbc6d98a7
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-10-21 22:33:28 -0700

    Update text.js

--- M05/text.js
+++ M05/text.js
@@ -2,7 +2,7 @@ var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';      // Message
 
 function myFunction(){
-  var num = prompt("Please enter a number:");
+  var num = prompt("Please enter a number between 0 and 10:");
   if (num>0 && num<10){
   while(i<11){
    msg += i+'x'+num+'='+(i*num)+'<br/>';
