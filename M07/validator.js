02dbb81874d47427a63802c2d176ad131772cb04
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-11-03 23:35:17 -0700

    Update validator.js

--- M07/validator.js
+++ M07/validator.js
@@ -1,6 +1,5 @@
 function validateForm() {
-    if (firstName() 
-    ){
+    if (firstName()){
     return true;
     }else{
         document.getElementById("errorMessages").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
@@ -9,7 +8,7 @@ function validateForm() {
 }
 
 FirstName.addEventListener('blur', firstName, false);
-function firstName(){
+function validateFirstname(){
     //1) Create variable
     var validFirstname=false;
 
@@ -49,7 +48,7 @@ if (lastName()){
   }
 
 LastName.addEventListener('blur', lastName, false);
-function lastName(){
+function validateLastname(){
     var validLastname=false;
 
     var lastname = document.getElementById("LastName").value;
@@ -80,8 +79,8 @@ function lastName(){
   }
   
   email.addEventListener('blur', email, false);
-  function email(){
-   var userEmail = document.getElementById("email").value;
+  function validateEmail(){
+   var userEmail = document.getElementById("Email").value;
    var atpos = userEmail.indexOf("@");
  var dotpos = userEmail.lastIndexOf(".");
 if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
@@ -98,12 +97,67 @@ if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
         return false;
   }
     phone.addEventListener('blur', phone, false);
-  function phone(){
-    var phone = document.getElementById("phone").value;
-if (isNaN(phone) || phone.lenght >15 || phone===null || phone===""){
+  function validatePhone(){
+    var phone = document.getElementById("Phone").value;
+if (isNaN(phone) || phone.length >15 || phone===null || phone===""){
  errorMessages = "<p>Invalid phone number！ </p>";
  
 }else{
   return true; 
 }
   }
+ if (username()){
+   return true;
+ }else{
+   document.getElementById("errorMessages").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
+        event.preventDefault();
+        return false;
+  }
+    username.addEventListener('blur', username, false);
+   function validateUsername(){
+      var username= document.getElementById("Username").value;
+ if (username===""|| username===null||username.length >12){
+   errorMessages ="<p> Invalid username! Username must include no more than 12 characters.</p>";
+   } else{
+     return true;
+   }
+ }
+ if (password()){
+   return true;
+ }else{
+   document.getElementById("errorMessages").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
+        event.preventDefault();
+        return false;
+  }
+    password.addEventListener('blur', password, false);
+   function validatePassword(){
+      var password= document.getElementById("Password").value;
+ if (password===""|| password===null||password.length >7){
+   errorMessages ="<p> Invalid password! Password must include no more than 7 characters </p>";
+   } else{
+     return true;
+   }
+ }
+ if (zipcode()){
+      return true;
+ }else{
+       document.getElementById("errorMessages").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
+        event.preventDefault();
+        return false;
+ }    
+     zipcode.addEventListener('blur', zipcode, false);
+     var country= document.getElementById("Country").value;   
+     if (country ==="US"){
+     function validateZipcode(){
+        var zipcode= document.getElememtById("zipcode").value;
+       if (zipcode.length != 5){
+         errorMessages= "<p> Invalid ZipCode! ZipCode must be 5 digits."
+       } else{
+         return true;
+       }
+     }else{
+         ZipCode.removeAttribute('required');
+       }
+      }
+
+
