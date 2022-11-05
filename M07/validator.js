10dc48fcee16aa8b6adab6f440e2ba941930e1d8
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-11-04 23:36:15 -0700

    Update validator.js

--- M07/validator.js
+++ M07/validator.js
@@ -1,108 +1,73 @@
-function validateForm() {
-    if (FirstName() && LastName() && Email() && Phone() && Username() && Password() && Address() && City() && State() && Country() && ZipCode()) {
-        return true;
+function valiateForm(){
+
+ var firstname= document.getElementById('FirstName').value;
+ var lastname= document.getElementById('LastName').value;
+ var email= document.getElementById('Email').value;
+ var phone= document.getElementById('Phone').value;
+ var username= document.getElementById('Username').value;
+ var password= document.getElementById('Password').value;
+ var address= document.getElementById('Address').value;
+ var city= document.getElementById('City').value;
+ var state= document.getElementById('State').value;
+ var country= document.getElementById('Country').value;
+ var zipcode= document.getElementById('ZipCpde').value;                 
+ var error=[];
+  
+  if (firstname ==="null" || firstname === "" || firstname.length > 20 ) {
+     error.push("First name invalid -length. It must be less than 20 characters.");
+     return false;
+   } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
+         error.push("Invaid characters in first name field.")
+       return false;       
+   } else {
+           return true;   
+   }
+  
+  if (lastname ==="null" || lastname ==="" || lastname.length > 50 ) {
+     error.push("Last name invalid -length. It must be less than 50 characters");
+     return false;
+   } else if (lastname.match("^[a-zA-Z ,.'-]+$")===null) {
+     error.push("Invaid characters in last name field.")
+       return false;       
+   } else {
+       return true;   
+   }
+  
+  var atpos = email.indexOf("@");
+  var dotpos = email.lastIndexOf(".");
+  if (atpos<1|| dotpos<atpos+2|| dotpos+2>=email.length){
+     error.push("Invaid Email address!")
+  }else{
+    return true;
+  }
+  
+  if (isNaN(phone) || phone.length > 15 || phone === null || phone === "") {
+    error.push="Invalid phone number!";
+  }else{
+    return true;
+  }
+  
+  if (username === "" || username === null || username.length > 12) {
+    error.push = "Invalid username! Username must include no more than 12 characters.";
     } else {
-        document.getElementById("errorMessages").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
-        event.preventDefault();
-        return false;
+    return true;
     }
-
-    FirstName.addEventListener("blur", FirstName, false);
-    LastName.addEventListener("blur", LastName, false);
-    Email.addEventListener("blur", Email, false);
-    Username.addEventListener('blur', Username, false);
-    Password.addEventListener("blur", Password, false);
-    ZipCode.addEventListener("blur", ZipCode, false);
-    Phone.addEventListener("blur", Phone, false);
-
-    function validateFirstname() {
-        var validFirstname = false;
-        var firstname = document.getElementById("FirstName").value;
-        var errorMessages = "";
-        if (firstname === "null" || firstname === "" || firstname.length > 20) {
-            errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
-            console.log("First name invalid — length")
-        } else if {
-            (firstname.match("^[a-zA-Z ,.'-]+$") === null errorMessages += "<p>Invalid character in first name (accepts only A-Z, a-z, and ,.'-)</p>"; console.log("First name invalid — bad characters");
-            }
-            else {
-                validFirstname = true;
-                console.log("First name valid");
-            }
-            document.getElementById("FirstName").innerHTML = errorMessages;
-            return (validFirstname);
-        }
+  
+  if (password === "" || password === null || password.length > 7) {
+    error.push = "Invalid password! Password must include no more than 7 characters";
+    } else {
+    return true;
     }
-
-    function validateLastname() {
-        var validLastname = false;
-
-        var lastname = document.getElementById("LastName").value;
-        var errorMessages = "";
-
-        if (lastname === "null" || lastname === "" || lastname.length > 50) {
-            errorMessages += "<p>The last name is required and cannot be greater than 50 characters</p>";
-            console.log("Last name invalid — length")
-        } else if (lastname.match("^[a-zA-Z ,.'-]+$") === null) {
-            errorMessages += "<p>Invalid character in last name (accepts only A-Z, a-z, and ,.'-)</p>";
-            console.log("Last name invalid — bad characters")
+  if (country === "US") {
+      if (zipcode.length != 5) {
+          error.push = "Invalid ZipCode. ZipCode must be 5 digits."
         } else {
-            validlastName = true;
-            console.log("Last name valid")
-        }
-
-        document.getElementById("LastName").innerHTML = errorMessages;
-
-        return (validLastname);
-    }
-}
-
-function validateEmail() {
-    var userEmail = document.getElementById("Email").value;
-    var atpos = userEmail.indexOf("@");
-    var dotpos = userEmail.lastIndexOf(".");
-    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length) {
-        errormessages = "<p>Invalid Email!</p>";
-    } else {
-        return true;
-    }
-}
-
-function validatePhone() {
-    var phone = document.getElementById("Phone").value;
-    if (isNaN(phone) || phone.length > 15 || phone === null || phone === "") {
-        errorMessages = "<p>Invalid phone number！ </p>";
-
-    } else {
-        return true;
-    }
-}
-
-function validateUsername() {
-    var username = document.getElementById("Username").value;
-    if (username === "" || username === null || username.length > 12) {
-        errorMessages = "<p> Invalid username! Username must include no more than 12 characters.</p>";
-    } else {
-        return true;
-    }
-}
-
-function validatePassword() {
-    var password = document.getElementById("Password").value;
-    if (password === "" || password === null || password.length > 7) {
-        errorMessages = "<p> Invalid password! Password must include no more than 7 characters </p>";
-    } else {
-        return true;
-        var country = document.getElementById("Country").value;
-        if (country === "US") {
-            function validateZipcode() {
-                var zipcode = document.getElememtById("zipcode").value;
-                if (zipcode.length != 5) {
-                    errorMessages = "<p> Invalid ZipCode! ZipCode must be 5 digits."
-                } else {
                     return true;
-                }
+         }
             } else {
-                ZipCode.removeAttribute("required");
+                zipcode.removeAttribute("required");
             }
-        }
+  
+  var errormessages=document.getElementById('errorMessages');
+  errormessages.innerText= error;
+}
