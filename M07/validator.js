df2c0b6aa7868367d911b33e8bb2143cfbac11cb
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-11-07 19:41:26 -0800

    Update validator.js

--- M07/validator.js
+++ M07/validator.js
@@ -1,5 +1,5 @@
 function validateForm(){
-
+  event.preventDefault();
  
   var firstname= document.getElementById("FirstName").value; 
   if (firstname ==="null" || firstname === "" || firstname.length > 20 ) {
