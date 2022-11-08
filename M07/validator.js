4218013fcc84cc5b4725ad047387e60d1ceb522d
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-11-07 21:18:45 -0800

    Update validator.js

--- M07/validator.js
+++ M07/validator.js
@@ -1,6 +1,7 @@
 function validateForm(){
   event.preventDefault();
- 
+  return false;
+}else if{
   var firstname= document.getElementById("FirstName").value; 
   if (firstname ==="null" || firstname === "" || firstname.length > 20 ) {
      document.getElementById("errorMessages").innerHTML ="First name invalid -length. It must be less than 20 characters.";
@@ -83,6 +84,6 @@ function validateForm(){
      document.getElementById("errorMessages").innerHTML = "Invalid ZipCode. ZipCode must be 5 digits."
      zipcode.focus();
      return false;
-  }
+  }else{
        return true;
          }
