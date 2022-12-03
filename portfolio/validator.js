e42bf2d4b6c4cac843009ea3ae4bfca800e1167f
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-12-03 14:14:32 -0800

    Update validator.js

--- portfolio/validator.js
+++ portfolio/validator.js
@@ -3,11 +3,11 @@ function validateForm(){
   var validFirstname=false;
   var firstname= document.getElementById("FirstName").value; 
   if (firstname ==="null" || firstname === "" || firstname.length > 20 ) {
-     document.getElementById("errorMessages").innerHTML ="First name invalid -length. It must be less than 20 characters.";
+     document.getElementById("errorMessages1").innerHTML ="First name invalid -length. It must be less than 20 characters.";
      document.getElementById("FirstName").focus();
      return false;
   }else if(firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-     document.getElementById("errorMessages").innerHTML ="Invaid characters in first name field.";
+     document.getElementById("errorMessages1").innerHTML ="Invaid characters in first name field.";
      document.getElementById("FirstName").focus();
      return false;
   }else{
@@ -17,11 +17,11 @@ function validateForm(){
    var validLastname=false;
    var lastname= document.getElementById("LastName").value;
   if (lastname ==="null" || lastname ==="" || lastname.length > 20 ) {
-    document.getElementById("errorMessages").innerHTML ="Last name invalid -length. It must be less than 20 characters";
+    document.getElementById("errorMessages2").innerHTML ="Last name invalid -length. It must be less than 20 characters";
      document.getElementById("LastName").focus();
      return false;
   } else if (lastname.match("^[a-zA-Z ,.'-]+$")===null) {
-    document.getElementById("errorMessages").innerHTML ="Invaid characters in last name field.";
+    document.getElementById("errorMessages2").innerHTML ="Invaid characters in last name field.";
      document.getElementById("LastName").focus();
      return false;
   } else {
@@ -33,7 +33,7 @@ function validateForm(){
   var atpos = email.indexOf("@");
   var dotpos = email.lastIndexOf(".");
   if (atpos<1|| dotpos<atpos+2|| dotpos+2>=email.length){
-     document.getElementById("errorMessages").innerHTML ="Invaid Email address!";
+     document.getElementById("errorMessages3").innerHTML ="Invaid Email address!";
      document.getElementById("Email").focus();
      return false;
   }else{
@@ -43,7 +43,7 @@ function validateForm(){
     var validComment=false;
   var comment= document.getElementById("Comment").value;
   if (comment === null || comment === "" || comment.length >400) {
-     document.getElementById("errorMessages").innerHTML = "The field must be filled out!";
+     document.getElementById("errorMessages4").innerHTML = "The field must be filled out!";
     document.getElementById("Comment").focus();
      return false;
     }else{
