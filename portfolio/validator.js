9653eeafa366603f811544f791c56de53035e70b
jy229319791 <114129690+jy229319791@users.noreply.github.com>
2022-12-03 14:01:59 -0800

    Update validator.js

--- portfolio/validator.js
+++ portfolio/validator.js
@@ -3,12 +3,12 @@ function validateForm(){
   var validFirstname=false;
   var firstname= document.getElementById("FirstName").value; 
   if (firstname ==="null" || firstname === "" || firstname.length > 20 ) {
-     document.getElementById("FirstName").focus();
      document.getElementById("errorMessages").innerHTML ="First name invalid -length. It must be less than 20 characters.";
+     document.getElementById("FirstName").focus();
      return false;
   }else if(firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-     document.getElementById("FirstName").focus();
      document.getElementById("errorMessages").innerHTML ="Invaid characters in first name field.";
+     document.getElementById("FirstName").focus();
      return false;
   }else{
     validFirstname=true;
@@ -17,12 +17,12 @@ function validateForm(){
    var validLastname=false;
    var lastname= document.getElementById("LastName").value;
   if (lastname ==="null" || lastname ==="" || lastname.length > 20 ) {
+    document.getElementById("errorMessages").innerHTML ="Last name invalid -length. It must be less than 20 characters";
      document.getElementById("LastName").focus();
-     document.getElementById("errorMessages").innerHTML ="Last name invalid -length. It must be less than 20 characters";
      return false;
   } else if (lastname.match("^[a-zA-Z ,.'-]+$")===null) {
+    document.getElementById("errorMessages").innerHTML ="Invaid characters in last name field.";
      document.getElementById("LastName").focus();
-     document.getElementById("errorMessages").innerHTML ="Invaid characters in last name field.";
      return false;
   } else {
      validLastname=true;
@@ -33,8 +33,8 @@ function validateForm(){
   var atpos = email.indexOf("@");
   var dotpos = email.lastIndexOf(".");
   if (atpos<1|| dotpos<atpos+2|| dotpos+2>=email.length){
-     document.getElementById("Email").focus();
      document.getElementById("errorMessages").innerHTML ="Invaid Email address!";
+     document.getElementById("Email").focus();
      return false;
   }else{
     validEmail=true;
@@ -43,8 +43,8 @@ function validateForm(){
     var validComment=false;
   var comment= document.getElementById("Comment").value;
   if (comment === null || comment === "" || comment.length >400) {
+     document.getElementById("errorMessages").innerHTML = "The field must be filled out!";
     document.getElementById("Comment").focus();
-    document.getElementById("errorMessages").innerHTML = "The field must be filled out!";
      return false;
     }else{
       validComment=true;
